717040de64e7f53f68cb08534a9c0b1f219d2769
stz2012 <tslroom@hotmail.com>
2018-12-04 15:02:49 +0900

    細部の修正

--- config.php
+++ config.php
@@ -66,7 +66,7 @@ define( 'DB_FILEPATH',  INSTALL_PATH.'/settings/epgrec.db' );	// ＤＢファイ
 // ライブラリのディレクトリをinclude_pathに追加
 $includes = array(INSTALL_PATH.'/classes', INSTALL_PATH.'/libs');
 $incPath = implode(PATH_SEPARATOR, $includes);
-require_once 'Smarty/Smarty.class.php';
+require_once 'autoload.php';
 require_once 'epgrecLib.inc.php';
 setlocale(LC_ALL, 'ja_JP.UTF-8');
 spl_autoload_register(function ($className) {
